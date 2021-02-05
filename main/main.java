2c360d83f46a83071f3b25591fa9dbafca918dfd
fourat-bs <mohamedfouratb@gmail.com>
2021-02-05 16:17:35 +0100

    rename

--- main/main.java
+++ main/main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package plze5dem;
+package main;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -15,7 +15,7 @@ import javafx.stage.Stage;
  *
  * @author moham
  */
-public class Plze5dem extends Application {
+public class main extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
